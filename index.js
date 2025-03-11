b3065513d20a2517be6277c9fb2484f36a80328b
Donnesca <163658888+Donnesca@users.noreply.github.com>
2025-03-11 15:28:29 +0200

    included error handling into function

--- index.js
+++ index.js
@@ -15,17 +15,32 @@ const fbr = 0.5; // fuel burn rate (kg/s)
 
 // Function to convert km/h to m/s
 const kmhToMs = (VelocityKmh) => {
+  if (typeof VelocityKmh !== "number") {
+    throw new Error("VelocityKmh must be number (km/h).");
+  }
   return VelocityKmh * (1000 / 3600);
 };
 
 // Function to convert m/s to km/h
 const msToKmh = (velocityMs) => {
+  if (typeof velocityMs !== "number") {
+    throw new Error("velocityMs must be a number (m/s).");
+  }
   return velocityMs * (3600 / 1000);
 };
 
 // Function to calculate new velocity
-const calcNewVel = (acceleration, initilalVelocityKmh, timeSeconds) => {
-  const initialVelocityMs = kmhToMs(initilalVelocityKmh);
+const calcNewVel = (acceleration, initialVelocityKmh, timeSeconds) => {
+  if (
+    typeof acceleration !== "number" ||
+    typeof initialVelocityKmh !== "number" ||
+    typeof timeSeconds !== "number"
+  ) {
+    throw new Error(
+      "Invalid input types. All parameters must be numbers. Acceleration (m/s^2), Velocity (km/h), TIme (seconds)."
+    );
+  }
+  const initialVelocityMs = kmhToMs(initialVelocityKmh);
   const newVelocityMs = initialVelocityMs + acceleration * timeSeconds;
   return msToKmh(newVelocityMs);
 };
@@ -36,9 +51,9 @@ const rf = fuel - fuelConsumed; // Calculates remaining fuel
 const vel2 = calcNewVel(acc, vel, time); //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (acc, vel, time) => {
-  return vel + acc * time;
-};
+//calcNewVel = (acc, vel, time) => {
+//return vel + acc * time;
+//};
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
